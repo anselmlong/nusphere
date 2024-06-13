1b063a0d4bc32dae5c1a4bacd9815e51964212ea
Anselm Long <anselmpius@gmail.com>
2024-06-13 21:02:59 +0800

    Changing name?

--- src/pages/myEvents.jsx
+++ src/pages/myEvents.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const MyEvents = ({ user }) => {
+const MyEvents = () => {
     return (
         <div
             style={{
@@ -10,11 +10,9 @@ const MyEvents = ({ user }) => {
                 height: "100vh",
             }}
         >
-            {user &&
-                <h1>
-                    {user.name}'s Events:
-                </h1>
-            }
+            <h1>
+                My Events:
+            </h1>
         </div>
     );
 };
